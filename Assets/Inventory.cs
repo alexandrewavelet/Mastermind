0ca7dd486e3104cdeeefd248861433187a7e2ed3
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2019-04-05 18:06:18 +0200

    Initial commit

---
+++ Assets/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour, IHasChanged
+{
+    [SerializeField] Transform slots;
+
+    // Use this for initialization
+    void Start()
+    {
+        HasChanged();
+    }
+
+    public void HasChanged()
+    {
+
+    }
+
+}
+
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
